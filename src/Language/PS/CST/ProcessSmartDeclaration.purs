b99cc24da9b42c2525e32a42ab085bb813e260e7
Serhii Khoma <srghma@gmail.com>
2020-07-22 16:56:07 +0300

    feat: SmartQualifiedName -> WIP

--- src/Language/PS/CST/ProcessSmartDeclaration.purs
+++ src/Language/PS/CST/ProcessSmartDeclaration.purs
@@ -11,6 +11,7 @@ import Control.Monad.State (State, modify_)
 import Data.Array as Array
 import Data.Maybe (Maybe(..))
 import Data.Maybe (fromMaybe) as Maybe
+import Data.Traversable (traverse)
 import Unsafe.Coerce (unsafeCoerce)
 
 type App = State (Array ImportDecl)
@@ -29,8 +30,11 @@ findOrNew find new array =
 
 ---------------------------------
 
-processDeclaration :: Declaration SmartQualifiedName -> Declaration QualifiedName
-processDeclaration (DeclData          { comments, head, constructors }) = DeclData { comments, head: processDataHead head, constructors: map processDataCtor constructors }
+processDeclaration :: Declaration SmartQualifiedName -> App (Declaration QualifiedName)
+processDeclaration (DeclData          { comments, head, constructors }) = do
+  (head' :: DataHead QualifiedName) <- processDataHead head
+  (constructors' :: Array (DataCtor QualifiedName)) <- traverse processDataCtor constructors
+  pure $ DeclData { comments, head: head', constructors: constructors' }
 processDeclaration (DeclType          { comments, head, type_ }) = unsafeCoerce unit
 processDeclaration (DeclNewtype       { comments, head, name }) = unsafeCoerce unit
 processDeclaration (DeclClass         { comments, head, methods }) = unsafeCoerce unit
@@ -41,129 +45,179 @@ processDeclaration (DeclValue         { comments, valueBindingFields }) = unsafe
 processDeclaration (DeclFixity        { comments, fixityFields }) = unsafeCoerce unit
 processDeclaration (DeclForeign       { comments, foreign_ }) = unsafeCoerce unit
 
-processDataHead :: DataHead SmartQualifiedName -> DataHead QualifiedName
-processDataHead (DataHead { dataHdName, dataHdVars }) = DataHead { dataHdName, dataHdVars: map processTypeVarBinding dataHdVars }
+processDataHead :: DataHead SmartQualifiedName -> App (DataHead QualifiedName)
+processDataHead (DataHead { dataHdName, dataHdVars }) = do
+  (dataHdVars' :: Array (TypeVarBinding QualifiedName)) <- traverse processTypeVarBinding dataHdVars
+  pure $ DataHead { dataHdName, dataHdVars: dataHdVars' }
 
-processKind :: Kind SmartQualifiedName -> Kind QualifiedName
-processKind (KindName prop) = KindName (processSmartQualifiedName prop)
-processKind (KindArr kindA kindB) = KindArr (processKind kindA) (processKind kindB)
-processKind (KindRow kindA) = KindRow (processKind kindA)
+processKind :: Kind SmartQualifiedName -> App (Kind QualifiedName)
+processKind (KindName prop) = KindName <$> processSmartQualifiedName prop
+processKind (KindArr kindA kindB) = KindArr <$> (processKind kindA) <*> (processKind kindB)
+processKind (KindRow kindA) = KindRow <$> (processKind kindA)
 
 processSmartQualifiedName :: SmartQualifiedName (ProperName ProperNameType_KindName) -> App (QualifiedName (ProperName ProperNameType_KindName))
-processSmartQualifiedName (SmartQualifiedName { module_, importType, name }) =
-  case importType of
+processSmartQualifiedName (SmartQualifiedName smartQualifiedName) =
+  case smartQualifiedName.importType of
        SmartQualifiedNameImportType__Full -> do
           modify_ $ findAndModifyOrNew
             (\(ImportDecl import_) ->
               case import_.qualification of
                    Nothing -> false
-                   Just moduleName -> module_ == moduleName
+                   Just moduleName -> smartQualifiedName.module_ == moduleName
             )
             (\(ImportDecl import_) ->
               ImportDecl $ import_
                 { names =
                   findOrNew
                   (case _ of
-                        ImportKind name' -> name' == name
+                        ImportKind name' -> name' == smartQualifiedName.name
                         _ -> false
                   )
-                  (\_ -> ImportKind name)
+                  (\_ -> ImportKind smartQualifiedName.name)
                   import_.names
                 }
             )
             (\_ -> ImportDecl
-              { moduleName: module_
-              , names: [ImportKind name]
-              , qualification: Just module_
+              { moduleName: smartQualifiedName.module_
+              , names: [ImportKind smartQualifiedName.name]
+              , qualification: Just smartQualifiedName.module_
               }
             )
-          pure $ QualifiedName { qualModule: Just module_, qualName: name }
+          pure $ QualifiedName { qualModule: Just smartQualifiedName.module_, qualName: smartQualifiedName.name }
        SmartQualifiedNameImportType__None -> do
-          pure $ QualifiedName { qualModule: Nothing, qualName: name }
+          modify_ $ findAndModifyOrNew
+            (\(ImportDecl import_) ->
+              case import_.qualification of
+                   Nothing -> import_.moduleName == smartQualifiedName.module_
+                   Just _ -> false
+            )
+            (\(ImportDecl import_) ->
+              ImportDecl $ import_
+                { names =
+                  findOrNew
+                  (case _ of
+                        ImportKind name' -> name' == smartQualifiedName.name
+                        _ -> false
+                  )
+                  (\_ -> ImportKind smartQualifiedName.name)
+                  import_.names
+                }
+            )
+            (\_ -> ImportDecl
+              { moduleName: smartQualifiedName.module_
+              , names: [ImportKind smartQualifiedName.name]
+              , qualification: Nothing
+              }
+            )
+          pure $ QualifiedName { qualModule: Nothing, qualName: smartQualifiedName.name }
        (SmartQualifiedNameImportType__Custom customModule) -> do
-          pure $ QualifiedName { qualModule: Just customModule, qualName: name }
+          modify_ $ findAndModifyOrNew
+            (\(ImportDecl import_) ->
+              case import_.qualification of
+                   Nothing -> false
+                   Just moduleName -> moduleName == customModule
+            )
+            (\(ImportDecl import_) ->
+              ImportDecl $ import_
+                { names =
+                  findOrNew
+                  (case _ of
+                        ImportKind name' -> name' == smartQualifiedName.name
+                        _ -> false
+                  )
+                  (\_ -> ImportKind smartQualifiedName.name)
+                  import_.names
+                }
+            )
+            (\_ -> ImportDecl
+              { moduleName: smartQualifiedName.module_
+              , names: [ImportKind smartQualifiedName.name]
+              , qualification: Just customModule
+              }
+            )
+          pure $ QualifiedName { qualModule: Just customModule, qualName: smartQualifiedName.name }
 
-processTypeVarBinding :: TypeVarBinding SmartQualifiedName -> TypeVarBinding QualifiedName
-processTypeVarBinding (TypeVarKinded ident kind_) = TypeVarKinded ident (processKind kind_)
-processTypeVarBinding (TypeVarName ident) = TypeVarName ident
+processTypeVarBinding :: TypeVarBinding SmartQualifiedName -> App (TypeVarBinding QualifiedName)
+processTypeVarBinding (TypeVarKinded ident kind_) = TypeVarKinded ident <$> (processKind kind_)
+processTypeVarBinding (TypeVarName ident) = pure $ TypeVarName ident
 
-processDataCtor :: DataCtor SmartQualifiedName -> DataCtor QualifiedName
+processDataCtor :: DataCtor SmartQualifiedName -> App (DataCtor QualifiedName)
 processDataCtor (DataCtor { dataCtorName, dataCtorFields }) = unsafeCoerce unit
 
-processType :: Type SmartQualifiedName -> Type QualifiedName
+processType :: Type SmartQualifiedName -> App (Type QualifiedName)
 processType _ = unsafeCoerce unit
 
-processInstanceHead :: InstanceHead SmartQualifiedName -> InstanceHead QualifiedName
+processInstanceHead :: InstanceHead SmartQualifiedName -> App (InstanceHead QualifiedName)
 processInstanceHead _ = unsafeCoerce unit
 
-processForeign :: Foreign SmartQualifiedName -> Foreign QualifiedName
+processForeign :: Foreign SmartQualifiedName -> App (Foreign QualifiedName)
 processForeign _ = unsafeCoerce unit
 
-processFixityFields :: FixityFields SmartQualifiedName -> FixityFields QualifiedName
+processFixityFields :: FixityFields SmartQualifiedName -> App (FixityFields QualifiedName)
 processFixityFields _ = unsafeCoerce unit
 
-processFixityOp :: FixityOp SmartQualifiedName -> FixityOp QualifiedName
+processFixityOp :: FixityOp SmartQualifiedName -> App (FixityOp QualifiedName)
 processFixityOp _ = unsafeCoerce unit
 
-processRow :: Row SmartQualifiedName -> Row QualifiedName
+processRow :: Row SmartQualifiedName -> App (Row QualifiedName)
 processRow _ = unsafeCoerce unit
 
-processConstraint :: Constraint SmartQualifiedName -> Constraint QualifiedName
+processConstraint :: Constraint SmartQualifiedName -> App (Constraint QualifiedName)
 processConstraint _ = unsafeCoerce unit
 
-processClassHead :: ClassHead SmartQualifiedName -> ClassHead QualifiedName
+processClassHead :: ClassHead SmartQualifiedName -> App (ClassHead QualifiedName)
 processClassHead _ = unsafeCoerce unit
 
-processValueBindingFields :: ValueBindingFields SmartQualifiedName -> ValueBindingFields QualifiedName
+processValueBindingFields :: ValueBindingFields SmartQualifiedName -> App (ValueBindingFields QualifiedName)
 processValueBindingFields _ = unsafeCoerce unit
 
-processBinder :: Binder SmartQualifiedName -> Binder QualifiedName
+processBinder :: Binder SmartQualifiedName -> App (Binder QualifiedName)
 processBinder _ = unsafeCoerce unit
 
-processGuarded :: Guarded SmartQualifiedName -> Guarded QualifiedName
+processGuarded :: Guarded SmartQualifiedName -> App (Guarded QualifiedName)
 processGuarded _ = unsafeCoerce unit
 
-processWhere :: Where SmartQualifiedName -> Where QualifiedName
+processWhere :: Where SmartQualifiedName -> App (Where QualifiedName)
 processWhere _ = unsafeCoerce unit
 
-processLetBinding :: LetBinding SmartQualifiedName -> LetBinding QualifiedName
+processLetBinding :: LetBinding SmartQualifiedName -> App (LetBinding QualifiedName)
 processLetBinding _ = unsafeCoerce unit
 
-processGuardedExpr :: GuardedExpr SmartQualifiedName -> GuardedExpr QualifiedName
+processGuardedExpr :: GuardedExpr SmartQualifiedName -> App (GuardedExpr QualifiedName)
 processGuardedExpr _ = unsafeCoerce unit
 
-processPatternGuard :: PatternGuard SmartQualifiedName -> PatternGuard QualifiedName
+processPatternGuard :: PatternGuard SmartQualifiedName -> App (PatternGuard QualifiedName)
 processPatternGuard _ = unsafeCoerce unit
 
-processExpr :: Expr SmartQualifiedName -> Expr QualifiedName
+processExpr :: Expr SmartQualifiedName -> App (Expr QualifiedName)
 processExpr _ = unsafeCoerce unit
 
-processRecordAccessor :: RecordAccessor SmartQualifiedName -> RecordAccessor QualifiedName
+processRecordAccessor :: RecordAccessor SmartQualifiedName -> App (RecordAccessor QualifiedName)
 processRecordAccessor _ = unsafeCoerce unit
 
-processRecordUpdate :: RecordUpdate SmartQualifiedName -> RecordUpdate QualifiedName
+processRecordUpdate :: RecordUpdate SmartQualifiedName -> App (RecordUpdate QualifiedName)
 processRecordUpdate _ = unsafeCoerce unit
 
-processLambda :: Lambda SmartQualifiedName -> Lambda QualifiedName
+processLambda :: Lambda SmartQualifiedName -> App (Lambda QualifiedName)
 processLambda _ = unsafeCoerce unit
 
-processIfThenElse :: IfThenElse SmartQualifiedName -> IfThenElse QualifiedName
+processIfThenElse :: IfThenElse SmartQualifiedName -> App (IfThenElse QualifiedName)
 processIfThenElse _ = unsafeCoerce unit
 
-processCaseOf :: CaseOf SmartQualifiedName -> CaseOf QualifiedName
+processCaseOf :: CaseOf SmartQualifiedName -> App (CaseOf QualifiedName)
 processCaseOf _ = unsafeCoerce unit
 
-processLetIn :: LetIn SmartQualifiedName -> LetIn QualifiedName
+processLetIn :: LetIn SmartQualifiedName -> App (LetIn QualifiedName)
 processLetIn _ = unsafeCoerce unit
 
-processDoStatement :: DoStatement SmartQualifiedName -> DoStatement QualifiedName
+processDoStatement :: DoStatement SmartQualifiedName -> App (DoStatement QualifiedName)
 processDoStatement _ = unsafeCoerce unit
 
-processAdoBlock :: AdoBlock SmartQualifiedName -> AdoBlock QualifiedName
+processAdoBlock :: AdoBlock SmartQualifiedName -> App (AdoBlock QualifiedName)
 processAdoBlock _ = unsafeCoerce unit
 
-processInstanceBinding :: InstanceBinding SmartQualifiedName -> InstanceBinding QualifiedName
+processInstanceBinding :: InstanceBinding SmartQualifiedName -> App (InstanceBinding QualifiedName)
 processInstanceBinding _ = unsafeCoerce unit
 
-processInstance :: Instance SmartQualifiedName -> Instance QualifiedName
+processInstance :: Instance SmartQualifiedName -> App (Instance QualifiedName)
 processInstance _ = unsafeCoerce unit
