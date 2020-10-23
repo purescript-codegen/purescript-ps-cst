776749a3e0e26718183826d9e652e364f1f06305
Jan Vincent Liwanag <jvliwanag@gmail.com>
2020-10-23 12:19:13 +0800

    Restore generic, eq, ord, and show instances

--- src/Language/PS/CST/Types/Declaration.purs
+++ src/Language/PS/CST/Types/Declaration.purs
@@ -1,10 +1,14 @@
 module Language.PS.CST.Types.Declaration where
 
+import Prelude
 
 import Data.Array.NonEmpty (NonEmptyArray)
 import Data.Either (Either)
-import Data.Maybe (Maybe)
 import Data.Either.Nested (type (\/))
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
+import Data.Maybe (Maybe)
+import Data.Newtype (class Newtype)
 import Language.PS.CST.Types.Leafs (ClassFundep, Comments, DeclDeriveType, Fixity, Ident, Label, OpName, OpNameType_TypeOpName, OpNameType_ValueOpName, ProperName, ProperNameType_ClassName, ProperNameType_ConstructorName, ProperNameType_KindName, ProperNameType_TypeName, RecordLabeled)
 import Language.PS.CST.Types.QualifiedName (QualifiedName)
 
@@ -56,10 +60,11 @@ data Declaration
     { comments :: Maybe Comments
     , foreign_ :: Foreign
     }
--- | derive instance genericDeclaration :: Generic Declaration _
--- | derive instance eqDeclaration :: Eq Declaration
--- | derive instance ordDeclaration :: Ord Declaration
--- instance showDeclaration :: Show Declaration where show = genericShow
+
+derive instance genericDeclaration :: Generic Declaration _
+derive instance eqDeclaration :: Eq Declaration
+derive instance ordDeclaration :: Ord Declaration
+instance showDeclaration :: Show Declaration where show = genericShow
 
 type InstanceHead =
   { instName :: Ident
@@ -72,9 +77,11 @@ data Foreign
   = ForeignValue { ident :: Ident, type_ :: Type }
   | ForeignData { name :: ProperName ProperNameType_TypeName, kind_ :: Kind }
   | ForeignKind { name :: ProperName ProperNameType_KindName }
--- | derive instance genericForeign :: Generic Foreign _
--- | derive instance eqForeign :: Eq Foreign
--- | derive instance ordForeign :: Ord Foreign
+
+derive instance genericForeign :: Generic Foreign _
+derive instance eqForeign :: Eq Foreign
+derive instance ordForeign :: Ord Foreign
+instance showForeign :: Show Foreign where show = genericShow
 
 type FixityFields =
   { keyword :: Fixity
@@ -85,9 +92,11 @@ type FixityFields =
 data FixityOp
   = FixityValue (QualifiedName Ident \/ QualifiedName (ProperName ProperNameType_ConstructorName)) (OpName OpNameType_ValueOpName)
   | FixityType (QualifiedName (ProperName ProperNameType_TypeName)) (OpName OpNameType_TypeOpName)
--- | derive instance genericFixityOp :: Generic FixityOp _
--- | derive instance eqFixityOp :: Eq FixityOp
--- | derive instance ordFixityOp :: Ord FixityOp
+
+derive instance genericFixityOp :: Generic FixityOp _
+derive instance eqFixityOp :: Eq FixityOp
+derive instance ordFixityOp :: Ord FixityOp
+instance showFixityOp :: Show FixityOp where show = genericShow
 
 data Type
   = TypeVar Ident
@@ -109,49 +118,53 @@ data Type
   -- | TypeOpName (QualifiedName (OpName OpNameType_TypeOpName))
   -- | TypeArrName
   -- | TypeParens Type
--- | derive instance genericType :: Generic Type _
--- | derive instance eqType :: Eq Type
--- | derive instance ordType :: Ord Type
--- instance showType :: Show Type where show = genericShow
+
+derive instance genericType :: Generic Type _
+derive instance eqType :: Eq Type
+derive instance ordType :: Ord Type
+instance showType :: Show Type where show x = genericShow x
 
 data Kind
   = KindName (QualifiedName (ProperName ProperNameType_KindName))
   | KindArr Kind Kind
   | KindRow Kind
   -- | KindParens Kind -- no need
--- | derive instance genericKind :: Generic Kind _
--- | derive instance eqKind :: Eq Kind
--- | derive instance ordKind :: Ord Kind
--- instance showKind :: Show Kind where
---   show _ = ""
+
+derive instance genericKind :: Generic Kind _
+derive instance eqKind :: Eq Kind
+derive instance ordKind :: Ord Kind
+instance showKind :: Show Kind where show x = genericShow x
 
 data TypeVarBinding
   = TypeVarKinded Ident Kind
   | TypeVarName Ident
--- | derive instance genericTypeVarBinding :: Generic TypeVarBinding _
--- | derive instance eqTypeVarBinding :: Eq TypeVarBinding
--- | derive instance ordTypeVarBinding :: Ord TypeVarBinding
--- instance showTypeVarBinding :: Show TypeVarBinding where show = genericShow
+
+derive instance genericTypeVarBinding :: Generic TypeVarBinding _
+derive instance eqTypeVarBinding :: Eq TypeVarBinding
+derive instance ordTypeVarBinding :: Ord TypeVarBinding
+instance showTypeVarBinding :: Show TypeVarBinding where show = genericShow
 
 newtype DataHead = DataHead
   { dataHdName :: ProperName ProperNameType_TypeName
   , dataHdVars :: Array TypeVarBinding
   }
--- | derive instance newtypeDataHead :: Newtype DataHead _
--- | derive instance genericDataHead :: Generic DataHead _
--- | derive instance eqDataHead :: Eq DataHead
--- | derive instance ordDataHead :: Ord DataHead
--- instance showDataHead :: Show DataHead where show = genericShow
+
+derive instance newtypeDataHead :: Newtype DataHead _
+derive instance genericDataHead :: Generic DataHead _
+derive instance eqDataHead :: Eq DataHead
+derive instance ordDataHead :: Ord DataHead
+instance showDataHead :: Show DataHead where show = genericShow
 
 newtype DataCtor = DataCtor
   { dataCtorName :: ProperName ProperNameType_ConstructorName
   , dataCtorFields :: Array Type
   }
--- | derive instance newtypeDataCtor :: Newtype DataCtor _
--- | derive instance genericDataCtor :: Generic DataCtor _
--- | derive instance eqDataCtor :: Eq DataCtor
--- | derive instance ordDataCtor :: Ord DataCtor
--- instance showDataCtor :: Show DataCtor where show = genericShow
+
+derive instance newtypeDataCtor :: Newtype DataCtor _
+derive instance genericDataCtor :: Generic DataCtor _
+derive instance eqDataCtor :: Eq DataCtor
+derive instance ordDataCtor :: Ord DataCtor
+instance showDataCtor :: Show DataCtor where show = genericShow
 
 type Row =
   { rowLabels :: Array { label :: Label, type_ :: Type }
@@ -164,10 +177,11 @@ newtype Constraint
     , args :: Array Type
     }
   -- | ConstraintParens Constraint
--- | derive instance genericConstraint :: Generic Constraint _
--- | derive instance eqConstraint :: Eq Constraint
--- | derive instance ordConstraint :: Ord Constraint
--- instance showConstraint :: Show Constraint where show = genericShow
+
+derive instance genericConstraint :: Generic Constraint _
+derive instance eqConstraint :: Eq Constraint
+derive instance ordConstraint :: Ord Constraint
+instance showConstraint :: Show Constraint where show = genericShow
 
 -- Delimeted or separated
 type ClassHead =
@@ -198,17 +212,19 @@ data Binder
   | BinderOp Binder (QualifiedName (OpName OpNameType_ValueOpName)) Binder
   -- | BinderParens Binder -- no need
 
--- | derive instance genericBinder :: Generic Binder _
--- | derive instance eqBinder :: Eq Binder
--- | derive instance ordBinder :: Ord Binder
--- instance showBinder :: Show Binder where show = genericShow
+derive instance genericBinder :: Generic Binder _
+derive instance eqBinder :: Eq Binder
+derive instance ordBinder :: Ord Binder
+instance showBinder :: Show Binder where show x = genericShow x
 
 data Guarded
   = Unconditional (Where)
   | Guarded (NonEmptyArray GuardedExpr)
--- | derive instance genericGuarded :: Generic Guarded _
--- | derive instance eqGuarded :: Eq Guarded
--- | derive instance ordGuarded :: Ord Guarded
+
+derive instance genericGuarded :: Generic Guarded _
+derive instance eqGuarded :: Eq Guarded
+derive instance ordGuarded :: Ord Guarded
+instance showGuarded :: Show Guarded where show x = genericShow x
 
 type Where =
   { expr :: Expr
@@ -219,9 +235,11 @@ data LetBinding
   = LetBindingSignature { ident :: Ident, type_ :: Type }
   | LetBindingName ValueBindingFields
   | LetBindingPattern { binder :: Binder, where_ :: Where }
--- | derive instance genericLetBinding :: Generic LetBinding _
--- | derive instance eqLetBinding :: Eq LetBinding
--- | derive instance ordLetBinding :: Ord LetBinding
+
+derive instance genericLetBinding :: Generic LetBinding _
+derive instance eqLetBinding :: Eq LetBinding
+derive instance ordLetBinding :: Ord LetBinding
+instance showLetBinding :: Show LetBinding where show x = genericShow x
 
 type GuardedExpr =
   { patterns :: NonEmptyArray PatternGuard
@@ -259,9 +277,11 @@ data Expr
   | ExprDo (NonEmptyArray DoStatement)
   | ExprAdo AdoBlock
   -- | ExprParens Expr -- no need
--- | derive instance genericExpr :: Generic Expr _
--- | derive instance eqExpr :: Eq Expr
--- | derive instance ordExpr :: Ord Expr
+
+derive instance genericExpr :: Generic Expr _
+derive instance eqExpr :: Eq Expr
+derive instance ordExpr :: Ord Expr
+instance showExpr :: Show Expr where show x = genericShow x
 
 type RecordAccessor =
   { recExpr :: Expr
@@ -271,9 +291,11 @@ type RecordAccessor =
 data RecordUpdate
   = RecordUpdateLeaf Label Expr
   | RecordUpdateBranch Label (NonEmptyArray RecordUpdate)
--- | derive instance genericRecordUpdate :: Generic RecordUpdate _
--- | derive instance eqRecordUpdate :: Eq RecordUpdate
--- | derive instance ordRecordUpdate :: Ord RecordUpdate
+
+derive instance genericRecordUpdate :: Generic RecordUpdate _
+derive instance eqRecordUpdate :: Eq RecordUpdate
+derive instance ordRecordUpdate :: Ord RecordUpdate
+instance showRecordUpdate :: Show RecordUpdate where show x = genericShow x
 
 type Lambda =
   { binders :: NonEmptyArray Binder
@@ -300,9 +322,11 @@ data DoStatement
   = DoLet (NonEmptyArray LetBinding)
   | DoDiscard Expr
   | DoBind { binder :: Binder, expr :: Expr }
--- | derive instance genericDoStatement :: Generic DoStatement _
--- | derive instance eqDoStatement :: Eq DoStatement
--- | derive instance ordDoStatement :: Ord DoStatement
+
+derive instance genericDoStatement :: Generic DoStatement _
+derive instance eqDoStatement :: Eq DoStatement
+derive instance ordDoStatement :: Ord DoStatement
+instance showDoStatement :: Show DoStatement where show = genericShow
 
 type AdoBlock =
   { statements :: Array DoStatement
@@ -312,9 +336,11 @@ type AdoBlock =
 data InstanceBinding
   = InstanceBindingSignature { ident :: Ident, type_ :: Type }
   | InstanceBindingName ValueBindingFields
--- | derive instance genericInstanceBinding :: Generic InstanceBinding _
--- | derive instance eqInstanceBinding :: Eq InstanceBinding
--- | derive instance ordInstanceBinding :: Ord InstanceBinding
+
+derive instance genericInstanceBinding :: Generic InstanceBinding _
+derive instance eqInstanceBinding :: Eq InstanceBinding
+derive instance ordInstanceBinding :: Ord InstanceBinding
+instance showInstanceBinding :: Show InstanceBinding where show = genericShow
 
 type Instance =
   { head :: InstanceHead
